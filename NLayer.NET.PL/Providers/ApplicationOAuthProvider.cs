36c1d36ffffed7b90fbf43e5e77893bb1d500054
golardy <andy301086@gmail.com>
2017-04-03 23:28:58 +0300

    Add swagger for documentation in web.api

---
+++ NLayer.NET.PL/Providers/ApplicationOAuthProvider.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NLayer.NET.PL.Providers
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly ApplicationUserManager _userManager;        
+        private readonly string publicClientId;
+        //private readonly IUserService userService;
+        public ApplicationUserManager UserManager => _userManager;
+
+        public ApplicationOAuthProvider(string publicClientId, ApplicationUserManager userManager)
+        {
+            if (publicClientId == null)
+            {
+                throw new ArgumentNullException(nameof(publicClientId));
+            }
+
+            this.publicClientId = publicClientId;
+            _userManager = userManager;
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var user = await this._userManager.FindAsync(context.UserName, context.Password);            
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            ClaimsIdentity oAuthIdentity = await this._userManager.CreateIdentityAsync(user,
+                OAuthDefaults.AuthenticationType);
+            ClaimsIdentity cookieIdentity = await this._userManager.CreateIdentityAsync(user,
+                CookieAuthenticationDefaults.AuthenticationType);
+
+            AuthenticationProperties properties = CreateProperties(user.UserName);
+            AuthenticationTicket ticket = new AuthenticationTicket(oAuthIdentity, properties);
+            context.Validated(ticket);
+
+            context.Request.Context.Authentication.SignIn(cookieIdentity);
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            // Resource owner password credentials does not provide a client ID.
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            if (context.ClientId == this.publicClientId)
+            {
+                Uri expectedRootUri = new Uri(context.Request.Uri, "/");
+
+                if (expectedRootUri.AbsoluteUri == context.RedirectUri)
+                {
+                    context.Validated();
+                }
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        public static AuthenticationProperties CreateProperties(string userName)
+        {
+            IDictionary<string, string> data = new Dictionary<string, string>
+            {
+                { "userName", userName }
+            };
+
+            return new AuthenticationProperties(data);
+        }
+    }
+}
