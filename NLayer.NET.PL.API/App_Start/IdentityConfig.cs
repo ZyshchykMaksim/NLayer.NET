79d7d8d476011a421c6e2cb7247efa78e34093a4
Maxim Zyshchyk <maksim.zyshchyk@itrexgroup.com>
2017-04-04 13:19:56 +0300

    Migration identity configuration in  web.config

--- NLayer.NET.PL.API/App_Start/IdentityConfig.cs
+++ NLayer.NET.PL.API/App_Start/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -7,37 +8,58 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.DataProtection;
 using NLayer.NET.DBL;
 using NLayer.NET.DBL.Entities;
+using NLayer.NET.PL.API.Configurations;
 using NLayer.NET.PL.API.Models;
+using PasswordValidator = Microsoft.AspNet.Identity.PasswordValidator;
 
 namespace NLayer.NET.PL.API
 {
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class ApplicationUserManager : UserManager<User>
     {
-        public ApplicationUserManager(IUserStore<User> store)
-            : base(store)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="store"></param>
+        public ApplicationUserManager(IUserStore<User> store) : base(store)
         {
+
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
         {
+            // See in web.config
+            IdentityConfiguration identityConfiguration = (IdentityConfiguration)ConfigurationManager.GetSection("identityConfiguration");
+
             var manager = new ApplicationUserManager(new UserStore<User>(context.Get<AppDbContext>()));
+
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<User>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                AllowOnlyAlphanumericUserNames = identityConfiguration?.UserValidator?.AllowOnlyAlphanumericUserNames ?? UserValidatorElement.AllowOnlyAlphanumericUserNamesDefault,
+                RequireUniqueEmail = identityConfiguration?.UserValidator?.RequireUniqueEmail ?? UserValidatorElement.RequireUniqueEmailDefault
             };
+
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = identityConfiguration?.PasswordValidator?.RequiredLength ?? PasswordValidatorElement.RequiredLengthDefault,
+                RequireNonLetterOrDigit = identityConfiguration?.PasswordValidator?.RequireNonLetterOrDigit ?? PasswordValidatorElement.RequireNonLetterOrDigitDefault,
+                RequireDigit = identityConfiguration?.PasswordValidator?.RequireDigit ?? PasswordValidatorElement.RequireDigitDefault,
+                RequireLowercase = identityConfiguration?.PasswordValidator?.RequireLowercase ?? PasswordValidatorElement.RequireLowercaseDefault,
+                RequireUppercase = identityConfiguration?.PasswordValidator?.RequireUppercase ?? PasswordValidatorElement.RequireUppercaseDefault,
             };
+
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
---
+++ NLayer.NET.PL.API/Configurations/IdentityConfiguration.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace NLayer.NET.PL.API.Configurations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class IdentityConfiguration : ConfigurationSection
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [ConfigurationProperty("UserValidator")]
+        public UserValidatorElement UserValidator => ((UserValidatorElement)(base["UserValidator"]));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [ConfigurationProperty("PasswordValidator")]
+        public PasswordValidatorElement PasswordValidator => ((PasswordValidatorElement)(base["PasswordValidator"]));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class UserValidatorElement : ConfigurationElement
+    {
+        public const bool AllowOnlyAlphanumericUserNamesDefault = false;
+        public const bool RequireUniqueEmailDefault = true;
+
+        /// <summary>
+        /// Only allow [A-Za-z0-9@_] in UserNames
+        /// </summary>
+        [ConfigurationProperty("AllowOnlyAlphanumericUserNames", DefaultValue = AllowOnlyAlphanumericUserNamesDefault, IsRequired = true)]
+        public bool AllowOnlyAlphanumericUserNames
+        {
+            get { return ((bool)(base["AllowOnlyAlphanumericUserNames"])); }
+            set { base["AllowOnlyAlphanumericUserNames"] = value; }
+        }
+
+        /// <summary>
+        ///  If set, enforces that emails are non empty, valid, and unique
+        /// </summary>
+        [ConfigurationProperty("RequireUniqueEmail", DefaultValue = RequireUniqueEmailDefault, IsRequired = true)]
+        public bool RequireUniqueEmail
+        {
+            get { return ((bool)(base["RequireUniqueEmail"])); }
+            set { base["RequireUniqueEmail"] = value; }
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class PasswordValidatorElement : ConfigurationElement
+    {
+        public const bool RequireDigitDefault = false;
+        public const int RequiredLengthDefault = 6;
+        public const bool RequireLowercaseDefault = false;
+        public const bool RequireNonLetterOrDigitDefault = false;
+        public const bool RequireUppercaseDefault = false;
+
+        /// <summary>
+        ///  Require a digit ('0' - '9')
+        /// </summary>
+        [ConfigurationProperty("RequireDigit", DefaultValue = RequireDigitDefault, IsRequired = true)]
+        public bool RequireDigit
+        {
+            get { return ((bool)(base["RequireDigit"])); }
+            set { base["RequireDigit"] = value; }
+        }
+
+        /// <summary>
+        ///  Minimum required length
+        /// </summary>
+        [ConfigurationProperty("RequiredLength", DefaultValue = RequiredLengthDefault, IsRequired = true)]
+        public int RequiredLength
+        {
+            get { return ((int)(base["RequiredLength"])); }
+            set { base["RequiredLength"] = value; }
+        }
+
+        /// <summary>
+        /// Require a lower case letter ('a' - 'z')
+        /// </summary>
+        [ConfigurationProperty("RequireLowercase", DefaultValue = RequireLowercaseDefault, IsRequired = true)]
+        public bool RequireLowercase
+        {
+            get { return ((bool)(base["RequireLowercase"])); }
+            set { base["RequireLowercase"] = value; }
+        }
+
+
+        /// <summary>
+        /// Require a non letter or digit character
+        /// </summary>
+        [ConfigurationProperty("RequireNonLetterOrDigit", DefaultValue = RequireNonLetterOrDigitDefault, IsRequired = true)]
+        public bool RequireNonLetterOrDigit
+        {
+            get { return ((bool)(base["RequireNonLetterOrDigit"])); }
+            set { base["RequireNonLetterOrDigit"] = value; }
+        }
+
+
+        /// <summary>
+        /// Require an upper case letter ('A' - 'Z')
+        /// </summary>
+        [ConfigurationProperty("RequireUppercase", DefaultValue = RequireUppercaseDefault, IsRequired = true)]
+        public bool RequireUppercase
+        {
+            get { return ((bool)(base["RequireUppercase"])); }
+            set { base["RequireUppercase"] = value; }
+        }
+    }
+}
