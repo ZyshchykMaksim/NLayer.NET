1f6cc7ed0e1825e3cf32637a75887bd132343bdb
Maxim Zyshchyk <maksim.zyshchyk@itrexgroup.com>
2017-04-05 16:42:04 +0300

    Configuration logger and redirect to swagger

--- NLayer.NET.PL.API/Controllers/HomeController.cs
+++ NLayer.NET.PL.API/Controllers/HomeController.cs
@@ -10,9 +10,7 @@ namespace NLayer.NET.PL.API.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
-
-            return View();
+            return new RedirectResult("~/swagger/ui/index");
         }
     }
 }
