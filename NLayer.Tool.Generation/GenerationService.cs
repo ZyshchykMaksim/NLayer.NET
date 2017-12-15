a1d3a7b61ab4f34a9436dc7f0d937ebf0ba26f2d
maksim zyshchyk <amporioo@gmail.com>
2017-12-15 17:13:42 +0300

    ADD: Added value generator.

---
+++ NLayer.Tool.Generation/GenerationService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NLayer.Tool.Generation
+{
+    /// <summary>
+    /// This class allows you to randomly generate a value of specified length.
+    /// </summary>
+    public class GenerationService : IGenerationService
+    {
+        private readonly Random rd = new Random();
+
+        private const string SymbolsDefault = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_";
+
+        #region Implementation of IGenerationService
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// <c>Using symbols : abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_</c>
+        /// </summary>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        public Task<string> GenerationAsync(int length)
+        {
+            return Task.Run(() => Generation(SymbolsDefault, length));
+        }
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// </summary>
+        /// <param name="simbols">The symbols to generate a value.</param>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        public Task<string> GenerationAsync(string simbols, int length)
+        {
+            return Task.Run(() => Generation(simbols, length));
+        }
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// <c>Using symbols : abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_</c>
+        /// </summary>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        public string Generation(int length)
+        {
+            return Generation(SymbolsDefault, length);
+        }
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// </summary>
+        /// <param name="symbols">The symbols to generate a value.</param>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        public string Generation(string symbols, int length)
+        {
+            if (string.IsNullOrWhiteSpace(symbols))
+            {
+                throw new ArgumentNullException(nameof(symbols));
+            }
+
+            if (length <= 0)
+            {
+                throw new ArgumentNullException(nameof(length));
+            }
+
+            var chars = new char[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                chars[i] = symbols[rd.Next(0, symbols.Length)];
+            }
+
+            return new string(chars);
+        }
+        #endregion
+    }
+}
---
+++ NLayer.Tool.Generation/IGenerationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Tool.Generation
+{
+    /// <summary>
+    /// This interface allows you to randomly generate a value of specified length.
+    /// </summary>
+    public interface IGenerationService
+    {
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// <example>Using symbols : abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-</example>
+        /// </summary>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        string Generation(int length);
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// <example>Using symbols : abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-</example>
+        /// </summary>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        Task<string> GenerationAsync(int length);
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// </summary>
+        /// <param name="simbols">The symbols to generate a value.</param>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        string Generation(string simbols, int length);
+
+        /// <summary>
+        /// Allows you to randomly generate a value of specified length.
+        /// </summary>
+        /// <param name="simbols">The symbols to generate a value.</param>
+        /// <param name="length">The length a value.</param>
+        /// <returns></returns>
+        Task<string> GenerationAsync(string simbols, int length);
+    }
+}
